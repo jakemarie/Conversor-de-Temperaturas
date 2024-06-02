6f03f37072c1eb3bf00e3c879bf85e5aba2fb2ac
Jaqueline <jake.marie@hotmail.com>
2024-06-02 12:07:34 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor_deTemperaturas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celcius, farenraid, kelvin;
+
+            Console.WriteLine("CONVERSOR DE TEMPERATURAS");
+            Console.Write("Insira a temperatura em Ceucius: ");
+            celcius = double.Parse(Console.ReadLine());
+            Console.WriteLine(" ------------------------------");
+
+            // (celcius * 9 / 5) + 32 farenraid
+            farenraid = (celcius * 9 / 5) + 32;
+
+            // ceulcius + 273,15
+            kelvin = celcius + 273.15;
+
+            Console.WriteLine(celcius + " graus celcios = " + farenraid + "graus farenraid");
+            Console.WriteLine(celcius + " graus celcios = " + kelvin + "graus kelvin");
+            Console.WriteLine(" ------------------------------");
+            Console.ReadKey();
+
+
+        }
+    }
+}
